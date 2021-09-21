edd01380c85565f4fecf26a575ba08c3a5d30b7e
Maykon Ferreira <maykon.h.ferrero@gmail.com>
2021-09-21 19:51:39 -0400

    AppBank project

---
+++ Criando-uma-aplicação-de-transferência-bancária/AppBank/AppBank/Enum/AccountType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBank
+{
+    public enum AccountType
+    {
+        NaturalPerson =1 ,
+         LegalPerson =2
+
+    }
+}
