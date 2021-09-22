7896a8d11ffe6bfdd1a1507f872b5a9b808011f3
Maykon Ferreira <maykon.h.ferrero@gmail.com>
2021-09-22 11:54:38 -0400

    averaging class grades

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+public class minhaClasse
+{
+
+	public static void Main(string[] args)
+	{
+
+		//implente sua solução aqui
+		int quantidadeNotasValidas = 0;
+		double media = 0;
+		while (quantidadeNotasValidas < 2)
+		{
+
+			double valor = double.Parse(Console.ReadLine());
+
+			if (valor >= 0 && valor <= 10)
+			{
+
+				quantidadeNotasValidas++;
+				media += valor;
+
+			}
+			else
+			{
+				Console.WriteLine("nota invalida");
+
+			}
+
+		}
+		media = media / 2;
+		Console.WriteLine("media = " + media.ToString("F"));
+	}
+}
