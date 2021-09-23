3182ada719d6bc66f86400deeafc4d3014778c3d
Maykon Ferreira <maykon.h.ferrero@gmail.com>
2021-09-23 15:40:40 -0400

    Estrela da morte db

---
+++ EstrelaDaMorteDb/ControleAcessoEstrelaDaMorte.sql
@@ -0,0 +1,73 @@
+CREATE TABLE Planetas(
+IdPlantea int NOT NULL,
+Nome varchar(50) NOT NULL,
+Rotacao float NOT NULL,
+Orbita float NOT NULL,
+Diametro float NOT NULL,
+Clima varchar (50) NOT NULL,
+Populacao int NOT NULL,
+)
+GO
+ALTER TABLE Planetas ADD CONSTRAINT PK_Planetas PRIMARY KEY (IdPlantea);
+
+CREATE TABLE Naves(
+IdNave int NOT NULL,
+Nome varchar(100) NOT NULL,
+Modelo varchar(150) NOT NULL,
+Passageiros int NOT NULL,
+Carga float NOT NULL,
+Classe varchar (100) NOT NULL,
+)
+GO
+ALTER TABLE Naves ADD CONSTRAINT PK_Naves PRIMARY KEY (IdNave);
+
+
+
+CREATE TABLE Pilotos(
+IdPiloto int NOT NULL,
+Nome varchar(200) NOT NULL,
+AnoNascimento varchar(10) NOT NULL,
+IdPlaneta int NOT NULL,
+)
+GO
+ALTER TABLE Pilotos ADD CONSTRAINT PK_Pilotos PRIMARY KEY (IdPiloto);
+GO
+ALTER TABLE Pilotos ADD CONSTRAINT PK_Pilotos_Planetas FOREIGN KEY (IdPlaneta)
+REFERENCES Planetas (IdPlantea);
+
+
+
+CREATE TABLE PilotosNaves(
+IdPiloto int NOT NULL,
+IdNave int NOT NULL,
+FlagAutorizado bit NOT NULL,
+)
+GO
+ALTER TABLE PilotosNaves ADD CONSTRAINT PK_PilotosNaves PRIMARY KEY (IdPiloto,IdNave );
+
+GO
+ALTER TABLE PilotosNaves ADD CONSTRAINT FK_PilotosNaves_Pilotos FOREIGN KEY (IdPiloto)
+REFERENCES Pilotos (IdPiloto)
+
+GO
+ALTER TABLE PilotosNaves ADD CONSTRAINT FK_PilotosNaves_Naves FOREIGN KEY (IdNave)
+REFERENCES Naves (IdNave)
+
+GO
+ALTER TABLE PilotosNaves ADD CONSTRAINT DF_PilotosNaves_FlagAutorizado DEFAULT (1) FOR FlagAutorizado
+
+ 
+ CREATE TABLE HistoricoViagens(
+IdPiloto int NOT NULL,
+IdNave int NOT NULL,
+DtSaida datetime NOT NULL,
+DtChegada datetime NULL,
+)
+
+GO
+ALTER TABLE HistoricoViagens ADD CONSTRAINT FK_HistoricoViagens_PilotosNaves FOREIGN KEY (IdPiloto,IdNave)
+REFERENCES PilotosNaves (IdPiloto,IdNave)
+
+GO
+ALTER TABLE HistoricoViagens CHECK CONSTRAINT FK_HistoricoViagens_PilotosNaves 
+
